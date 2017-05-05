ddd5e2f1c8d6ccfe4064c15d1288c2547e494c46
Atsuya Ootsuka <ootsuka11@me.com>
2017-05-05 19:45:19 +0900

    3rd_commit

--- Program.cs
+++ Program.cs
@@ -10,9 +10,27 @@ namespace HorL
     {
         static void Main(string[] args)
         {
+            
+
+            // ランダムな数字を2つ作成する
+            // 生成したa1の値のみ表示させる
+            Random rnd = new Random();
+            int a1 = rnd.Next(10);
+            int a2 = rnd.Next(10);
+
+            Console.WriteLine(a1 + " : Hi or Low");
+            //Console.WriteLine(a2);
+
             //ユーザから値を受け取る
             var usersAnswer = Console.ReadLine();
-            Console.WriteLine(usersAnswer);
+
+
+
+            //ユーザから受け取った値を画面に出力
+            Console.WriteLine(a2);
+
+
+            //コンソールが終了しないようにするため
             Console.Read();
         }
     }
